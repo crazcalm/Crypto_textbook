ef6f2be51bd8d1976be3ace063c906e34109694c
crazcalm <crazcalm@gmail.com>
2014-12-21 10:20:44 -0500

    Setting up the boilerplate for the tests

---
+++ Classic_Cryptosystems/tests/test_cryptoMath.py
@@ -0,0 +1,9 @@
+import unittest
+import cryptoMath
+
+class Testing(unittest.TestCase):
+    def test_testing(self):
+        self.assertEqual(1,1)
+
+if __name__ == "__main__":
+    unittest.main()
