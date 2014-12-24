b8df2214899f89b58943556d9e6433b31b1b8147
Crazcalm <crazcalm@gmail.com>
2014-12-24 16:13:33 -0500

    Changing the interpretor to the standard python3

--- Classic_Cryptosystems/test_runner.py
+++ Classic_Cryptosystems/test_runner.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-print('Python3.4 tests')
-subprocess.call('python3.4 -m unittest discover tests/', shell=True)
+print('Python3 tests')
+subprocess.call('python3 -m unittest discover tests/', shell=True)
 print('\n\nPython2.7 tests')
 subprocess.call('python -m unittest discover tests/', shell=True)
